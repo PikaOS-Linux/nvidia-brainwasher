3a4adf6c879743a5e7503ecd0a3a7aacbc8508b1
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-19 20:02:16 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,12 @@ DRIVER=535
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
+echo 'Package: *' > /etc/apt/preferences.d/0-a
+echo 'Pin: release c=external' >> /etc/apt/preferences.d/0-a
+echo 'Pin-Priority: 1000' >> /etc/apt/preferences.d/0-a
+apt update -y
 apt download nvidia-driver-"$DRIVER"
+#wget https://ppa.launchpadcontent.net/graphics-drivers/ppa/ubuntu/pool/main/n/nvidia-graphics-drivers-535/nvidia-driver-535_535.113.01-0ubuntu3_amd64.deb
 ar -x ./nvidia-driver-"$DRIVER"*.deb
 mkdir -p ./nvidia-driver-"$DRIVER"/DEBIAN
 tar -xf ./control.tar.* -C ./nvidia-driver-"$DRIVER"/DEBIAN/
@@ -18,4 +23,7 @@ dpkg-deb --build ./nvidia-driver-"$DRIVER"/
 
 # Move the debs to output
 mkdir -p ./output
-mv ./*.deb ./output/
+for i in ./*.deb
+do
+  mv $i ./output/$i-"$(apt-cache show nvidia-kernel-source-$DRIVER | grep Version: | head -n1 | cut -f2 -d":" | tr -d ' ')".deb
+done
